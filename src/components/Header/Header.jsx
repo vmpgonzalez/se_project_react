0a8dbbbf3ae3146fa4c5f8da78c187a5b077f1b2
vmpgonzalez <vpacheco17@gmail.com>
2025-04-07 09:00:46 -0700

    Implement Add Clothes modal popup and fix modal toggle logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,26 @@
+import React from "react";
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
-function Header() {
+function Header({ weatherData, onAddClick }) {
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
-
   return (
     <header className="header">
       <div className="header__left">
         <img src={logo} alt="WTWR Logo" className="header__logo" />
         <div className="header__location-info">
-          <p className="header__date">{currentDate} • Seattle, WA</p>
+          <p className="header__date">{currentDate}, Seattle, WA</p>
         </div>
       </div>
 
       <div className="header__right">
-        <button className="header__button">+ Add Clothes</button>
+        <button className="header__button" onClick={onAddClick}>
+          + Add Clothes
+        </button>
         <p className="header__username">Victor</p>
         <img src={avatar} alt="User Avatar" className="header__avatar" />
       </div>
