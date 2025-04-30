c6a9730afaf6bb5ca13877a776f16a4d0a9583f5
vmpgonzalez <vpacheco17@gmail.com>
2025-04-30 09:05:03 -0700

    Finalize Sprint 10: Cleaned up styles, fixed modal UI, ensured deployment readiness

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 import { defaultClothingItems } from "../../utils/defaultClothingItems";
 import { getWeatherData } from "../../utils/weatherApi";
 
